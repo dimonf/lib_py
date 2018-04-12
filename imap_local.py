0fec34dd55d12ba7ecc05735a6445f380f73af0e
dimonf <camel109@gmail.com>
2018-04-12 17:38:26 +0300

    imap: print search result format improved

--- imap_local.py
+++ imap_local.py
@@ -1,8 +1,9 @@
-import imaplib
+import datetime
 import email
+import imaplib
 import os
-import sys
 import re
+import sys
 
 #requires python3. for python2 replace  message_from_bytes with message_from_string
 
@@ -17,7 +18,7 @@ class IMAP():
         self.con.login(user, password)
         self.con.select(box, readonly)
 
-    def search(self, imap_filter='UNSEEN', output='mail', query_str='(BODY.PEEK[HEADER])'):
+    def search(self, imap_filter='UNSEEN', output='str', query_str='(BODY.PEEK[HEADER])'):
         '''
         imap_filter:
             (SUBJECT "test message 2")
@@ -38,12 +39,28 @@ class IMAP():
 
         for id in data_s[0].split():
             resp, data = self.con.fetch(id, query_str)
-            email_body = data[0][1]
-            if output == 'mail':
-                mail = email.message_from_bytes(email_body)
-                msgs.append(mail)
-            elif output in ['str','text', 'txt']:
+            #output option for raw byte stream is discarded (no use for)
+            #hence, conversion to str is done unconditionally
+            email_body = email.message_from_bytes(data[0][1])
+
+            if output in ['str','text', 'txt', 'mail']:
                 msgs.append(email_body)
+            elif output in ['digest','print']:
+                # Now convert to local date-time
+                #stolen from here https://gist.github.com/robulouski/7441883
+                local_date_str = ''
+                date_tuple = email.utils.parsedate_tz(email_body['Date'])
+                if date_tuple:
+                     local_date = datetime.datetime.fromtimestamp(
+                         email.utils.mktime_tz(date_tuple))
+                     local_date_str = local_date.strftime("%Y-%b-%d %H:%M")
+                subject = str(email.header.make_header(
+                    email.header.decode_header(email_body['Subject'])))
+                #
+                msgs.append('{}#{}| {}'.format(
+                    local_date_str,
+                    id.decode('utf-8'),
+                    subject))
 
         return msgs
 
