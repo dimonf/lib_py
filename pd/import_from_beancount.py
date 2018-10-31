af23f16f7ea513dd13f9479d8bf9ed5cf1bac22e
dimonf <camel109@gmail.com>
2018-10-31 14:24:24 +0200

    WIP:beancount2dataframe convertor

---
+++ pd/import_from_beancount.py
@@ -0,0 +1,42 @@
+import csv
+import pandas as pd
+import re
+import datetime
+
+
+re_split_space = re.compile(r'\s+')
+
+def get_dataframe(csvdata, columns, delimeter=',', dtypes={}):
+    '''
+    csvdata shall not include column labels and index
+    columns: list of str values
+    dtypes: {index:keyword}
+            keywords: position, date
+    '''
+    records = []
+    flag_columns = True
+
+    for row in csv.reader(csvdata, skipinitialspace=True):
+        #remove spaces
+        for i in range(len(row)):
+            row[i] = row[i].strip()
+        #split "position" data type into two columns: amount and curr
+        for k,v in dtypes.items():
+            if v.count('position'):
+                try:
+                    amount,commodity =  re_split_space.split(row[k])
+                except IndexError:
+                    print('wrong position_types value')
+
+                row[k] = float(amount)
+                row.insert(k+1, commodity)
+                if flag_columns:
+                    columns.insert(k+1, columns[k] + "_curr")
+                    columns[k] = columns[k] + "_amount"
+                    flag_columns = False
+            elif v.count('date'):
+                date = datetime.date.fromisoformat(row[k])
+        records.append(row)
+
+    return pd.DataFrame(records, columns=columns)
+
