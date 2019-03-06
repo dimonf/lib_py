3644299ff0644b4d3f6b7bbb09195b082aecf198
dimonf <camel109@gmail.com>
2019-03-06 05:06:54 +0000

    new get_root_dir function
      While the hirerarchy of the files/dirs within working tree
      for jupyterlab installation is not predetermined and can have
      any number of levels, allowed by underlying file system, it is
      very convenient to get standardized access to files/folders,
      which contain resources, shared between all projects/notebooks.
      A file .jupyter_root_dir, placed at any directory, common to
      all projects, serves as indicator of 'root' directory, from where
      it is easy to locate shared resoruces, which location relating
      to this root shall remain static

--- utils/jup_utils.py
+++ utils/jup_utils.py
@@ -12,3 +12,21 @@ def get_kernel_id():
    with open('/tmp/jup_id','w') as f:
          f.write(kernel_id)
    return kernel_id
+
+def get_root_dir():
+    '''returns root (top) directory relative path from any location within it.
+       Some shared resources, such as bulky data files with imported records,
+       are stored it predetermined location, e.g. [root]/data. It is convenient 
+       to have unified access to such resources from code of any notebook, 
+       located at any level within hirearchy. The root dir is indicated by 
+       placement of a file .jupyter_root_dir into it'''
+    import os
+    
+    root_indicator = '.jupyter_root_dir'
+    next_up_dir = os.path.dirname(os.getcwd())
+    root_dir = None
+    while not root_dir:
+        next_up_dir = os.path.dirname(next_up_dir)
+        if os.path.exists(os.path.join(next_up_dir, root_indicator)):
+            root_dir = next_up_dir
+    return root_dir
